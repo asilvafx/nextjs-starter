950a876896c04be9cc57a0e945073e171b02fc8b
António Da Silva <73810823+asilvafx@users.noreply.github.com>
2025-09-17 20:29:49 +0200

    v1

--- src/components/ui/theme-mode.tsx
+++ src/components/ui/theme-mode.tsx
@@ -18,7 +18,7 @@ const SWITCH_DATA: SwitchOption[] = [
     name: 'System',
     value: 'system',
     iconSvg: (
-      <svg xmlns="http://www.w3.org/2000/svg" className="size-3" viewBox="0 0 24 24">
+      <svg xmlns="http://www.w3.org/2000/svg" className="size-4" viewBox="0 0 24 24">
         <path
           fill="currentColor"
           d="M1 2h22v8.25h-2V4H3v12h8.5v2H1zm2 18h8.5v2H3z"
@@ -34,7 +34,7 @@ const SWITCH_DATA: SwitchOption[] = [
     name: 'Light',
     value: 'light',
     iconSvg: (
-      <svg xmlns="http://www.w3.org/2000/svg" className="size-3" viewBox="0 0 24 24">
+      <svg xmlns="http://www.w3.org/2000/svg" className="size-4" viewBox="0 0 24 24">
         <path
           fill="none"
           stroke="currentColor"
@@ -50,7 +50,7 @@ const SWITCH_DATA: SwitchOption[] = [
     name: 'Dark',
     value: 'dark',
     iconSvg: (
-      <svg xmlns="http://www.w3.org/2000/svg" className="size-3" viewBox="0 0 24 24">
+      <svg xmlns="http://www.w3.org/2000/svg" className="size-4" viewBox="0 0 24 24">
         <path
           fill="none"
           stroke="currentColor"
