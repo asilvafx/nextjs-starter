294af5103f3f73f587b39076d5b076db245d302a
António Da Silva <73810823+asilvafx@users.noreply.github.com>
2025-09-17 20:25:12 +0200

    v1

--- src/components/ui/theme-mode.tsx
+++ src/components/ui/theme-mode.tsx
@@ -34,7 +34,7 @@ const SWITCH_DATA: SwitchOption[] = [
     name: 'Light',
     value: 'light',
     iconSvg: (
-      <svg xmlns="http://www.w3.org/2000/svg" className="size-5" viewBox="0 0 24 24">
+      <svg xmlns="http://www.w3.org/2000/svg" className="size-3" viewBox="0 0 24 24">
         <path
           fill="none"
           stroke="currentColor"
