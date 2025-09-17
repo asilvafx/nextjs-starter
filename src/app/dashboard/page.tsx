1c5a9882d8101922d8856785586eb9b9c16cdbb6
António Da Silva <73810823+asilvafx@users.noreply.github.com>
2025-09-17 16:03:17 +0200

    v1

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,7 +1,3 @@
-"use client";
-
-import { useAuth } from "@/hooks/useAuth"; 
-import { redirect } from "next/navigation"; 
 import { AppSidebar } from "@/components/app-sidebar"
 import {
   Breadcrumb,
@@ -11,24 +7,16 @@ import {
   BreadcrumbPage,
   BreadcrumbSeparator,
 } from "@/components/ui/breadcrumb"
-import { Separator } from "@/components/ui/separator" 
+import { Separator } from "@/components/ui/separator"
 import {
   SidebarInset,
   SidebarProvider,
   SidebarTrigger,
-} from '@/components/ui/sidebar'
-import { cn } from "@/lib/utils";
+} from "@/components/ui/sidebar"
 
-export default function AdminLayout({ children }) {
-  const { isAuthenticated, user, status } = useAuth();
-
-  // Protect admin routes
-  if (!isAuthenticated && status !== "loading") {
-    redirect("/auth/login");
-  } 
-
+export default function Page() {
   return (
-      <SidebarProvider>
+    <SidebarProvider>
       <AppSidebar />
       <SidebarInset>
         <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
@@ -53,10 +41,15 @@ export default function AdminLayout({ children }) {
             </Breadcrumb>
           </div>
         </header>
-        <div className="flex flex-1 flex-col gap-4 p-4 pt-0"> 
-           {children}
+        <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
+          <div className="grid auto-rows-min gap-4 md:grid-cols-3">
+            <div className="bg-muted/50 aspect-video rounded-xl" />
+            <div className="bg-muted/50 aspect-video rounded-xl" />
+            <div className="bg-muted/50 aspect-video rounded-xl" />
+          </div>
+          <div className="bg-muted/50 min-h-[100vh] flex-1 rounded-xl md:min-h-min" />
         </div>
       </SidebarInset>
     </SidebarProvider>
-  );
+  )
 }
